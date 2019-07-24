7ebab5f0d7a8e75b7a1b88c2bd937cbf2dfa26ad
Kayla <kaynuhe@gmail.com>
2019-07-24 16:18:24 -0400

    chore: add Sass folder, add API call in main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@ $(function() {
 
     // dropdown menu selection
 $('select').on('change', () => {
+    // save menu selection
     let $selection = $('select').val()
-    console.log($selection)
+    // console.log($selection)
+    $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=hbvVpmOEMRq8xxJSav79k8nlLeNRfUGg`)
+    .done(function(data) {
+        console.log("data", data)
+        $.each(data, function(key, value) {
+            console.log("value in data", value)
+            // append each result to a div with photo
+            // $('ul').append(`<li>${value.name}</li>`)
+            })
+    })
+    .fail(function() {})
+    .always(function(){})
 })
 
 
@@ -11,3 +23,22 @@ $('select').on('change', () => {
 
 
 });
+
+
+
+// $("button").on("click", function() {
+//     $.getJSON("https://api.nytimes.com/svc/topstories/v2/science.json?api-key=hbvVpmOEMRq8xxJSav79k8nlLeNRfUGg")
+//             // need to replace the /science from url above to selected item (use string interpolation)
+//     .done(function(data) {
+//       console.log(data)
+//       $.each(data, function(key, value) {
+//         $('ul').append(`<li>${value.name}</li>`)
+//       })
+//     })
+//     .fail(function() {
+//       alert('sorry, your request could not be processed') // in case the request didn't work
+//     })
+//     .always(function() {
+//       //something that happens whether request goes through or not
+//     })
+//   });
