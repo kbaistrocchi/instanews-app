d154a70f8354edae42389fc58c302a857109742c
Kayla <kaynuhe@gmail.com>
2019-07-25 15:44:24 -0400

    feat: get nyt top story titles and save to div

--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@ $('select').on('change', () => {
     $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=hbvVpmOEMRq8xxJSav79k8nlLeNRfUGg`)
     .done(function(data) {
         console.log("data", data)
-        $.each(data, function(key, value) {
-            console.log("value in data", value)
+        $.each(data.results, function(key, value) {
+            // console.log("value in data", value)
+            console.log(value.title)
             // append each result to a div with photo
-            // $('ul').append(`<li>${value.name}</li>`)
+            $('main').append(`<div><h2>${value.title}</h2></div>`)
+
             })
     })
     .fail(function() {})
@@ -24,21 +26,3 @@ $('select').on('change', () => {
 
 });
 
-
-
-// $("button").on("click", function() {
-//     $.getJSON("https://api.nytimes.com/svc/topstories/v2/science.json?api-key=hbvVpmOEMRq8xxJSav79k8nlLeNRfUGg")
-//             // need to replace the /science from url above to selected item (use string interpolation)
-//     .done(function(data) {
-//       console.log(data)
-//       $.each(data, function(key, value) {
-//         $('ul').append(`<li>${value.name}</li>`)
-//       })
-//     })
-//     .fail(function() {
-//       alert('sorry, your request could not be processed') // in case the request didn't work
-//     })
-//     .always(function() {
-//       //something that happens whether request goes through or not
-//     })
-//   });
