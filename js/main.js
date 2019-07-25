7fd7f12adadf2038877c1ca5a3557709c7338996
Kayla <kaynuhe@gmail.com>
2019-07-25 16:10:54 -0400

    feat: add eslint to gulp environment

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('select').on('change', () => {
     // console.log($selection)
     $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=hbvVpmOEMRq8xxJSav79k8nlLeNRfUGg`)
     .done(function(data) {
-        console.log("data", data)
+        console.log('data', data)
         $.each(data.results, function(key, value) {
             // console.log("value in data", value)
             console.log(value.title)
