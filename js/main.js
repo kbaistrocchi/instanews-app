6f640d7096dcfeac12c39c8e6beba791e68529f6
Kayla <kaynuhe@gmail.com>
2019-07-29 13:50:41 -0400

    style: shrink header when catagory selected in mobile

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ $('select').on('change', () => {
     // console.log($selection)
     $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?&max-results=5&api-key=hbvVpmOEMRq8xxJSav79k8nlLeNRfUGg`)
     .done(function(data) {
+        $('header').css('height', '300px')
+        $('.logo').css('width', '40%')
         console.log('data', data)
         let filteredData = []
         // filter results for only those with images
@@ -24,11 +26,6 @@ $('select').on('change', () => {
             let image = filteredData[i].multimedia[4].url
             let url = filteredData[i].url
 
-            // append each result to a div with photo
-            // wrap entire div in link to url 
-
-            // put title and abstract in another UN-NESTED div and overlay it using grid. Hopefully this will 
-            // fix the text overflow issue
         $('main').append(`<div class="top-story"><div class="img-wrapper"><img src="${image}"></div><div class="text"><a href="${url}"><h2>${title}</h2></a><p>${abstract}</p></div></div>`)
 
             }
